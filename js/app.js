91ad552ba1d83fe4339383a5cbc303e09af685ef
Oleksii Miroshko <miroshko@live.com>
2014-09-28 00:32:14 +0200

    requirejs based frontend

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
-import Field from 'Field';
-field = new Field(9, 9);
+require.config({
+  baseUrl: 'js',
+  paths: {
+    'lodash': '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min',
+    'vue': '//cdnjs.cloudflare.com/ajax/libs/vue/0.10.6/vue.min' 
+  }
+}); 
+
+
+require(['Field'], function(Field) {
+  field = new Field(9, 9);  
+});
