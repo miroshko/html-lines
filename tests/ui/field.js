a339ba405a201361bac8fa15b46fef06e593c633
Oleksii Miroshko <miroshko@live.com>
2014-10-08 00:21:57 +0200

    Switched to webdriver

--- tests/ui/field.js
+++ tests/ui/field.js
@@ -1,32 +1,26 @@
 var expect = require("chai").expect;
-var Browser = require('zombie');
+var test = require('selenium-webdriver/testing');
+var webdriver = require('selenium-webdriver');
 
-describe('Page with Lines field', function() {
-  var url = 'http://localhost:8000';
-  var browser;
+test.describe('Page with Lines field', function() {
+  var url = 'http://localhost:8000/test.html';
 
-  before(function(done) {   
-    // Math.random = require("../helpers/seeded_random")(50);
-    browser = new Browser();
-    browser.visit(url, function() {
-      browser.assert.success();
-      setTimeout(done, 1000);
-    });
-  });
-
-  it('sees field with initial balls on it', function() {
-    expect(browser.query("#field")).to.be.ok;
-
-    console.log(browser.body.innerHTML)
-
-    // for default values only
-    expect(browser.body.querySelectorAll('.tile')).to.have.length(81);
-    expect(browser.body.querySelectorAll('.tile.colored')).to.have.length(3);
-  });
-  it('can select and unselect a ball', function() {
+  test.it('sees field with initial balls on it', function() {
+    var driver = new webdriver.Builder().
+      withCapabilities(webdriver.Capabilities.firefox()).
+      build();
+    driver.get(url).then(function() {
+      driver.findElements(webdriver.By.className('tile')).then(function(tiles) {
+        expect(tiles).to.have.length(81);
+        
+      })
+    }).then(function() {
+      driver.quit();
+    });;
 
   });
-  it('can move selected ball')
-  it('cannot move unselected ball')
-  it('gets new balls after each turn')
+  test.it('can select and unselect a ball');
+  test.it('can move selected ball')
+  test.it('cannot move unselected ball')
+  test.it('gets new balls after each turn')
 });
